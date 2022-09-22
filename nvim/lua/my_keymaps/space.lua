3b19b1b47cc56a5d5d734f88327bc5764d17d2b8
Andy Zhang <half.a.tan@gmail.com>
2022-09-23 02:03:28 +0800

    Window number

--- nvim/lua/my_keymaps/space.lua
+++ nvim/lua/my_keymaps/space.lua
@@ -7,22 +7,22 @@ local function focus_winnr(nr)
     nr = nr + 1
   end
 
-  if vim.fn.winnr('$') > nr then
+  if vim.fn.winnr('$') >= nr then
     vim.api.nvim_command(nr .. 'wincmd w')
   end
 end
 
 wk.register({
   ['0'] = {'<cmd>NvimTreeFocus<cr>', 'Focus nvim-tree'},
-  ['1'] = {focus_winnr(1), 'Focus window 1'},
-  ['2'] = {focus_winnr(2), 'Focus window 2'},
-  ['3'] = {focus_winnr(3), 'Focus window 3'},
-  ['4'] = {focus_winnr(4), 'Focus window 4'},
-  ['5'] = {focus_winnr(5), 'Focus window 5'},
-  ['6'] = {focus_winnr(6), 'Focus window 6'},
-  ['7'] = {focus_winnr(7), 'Focus window 7'},
-  ['8'] = {focus_winnr(8), 'Focus window 8'},
-  ['9'] = {focus_winnr(9), 'Focus window 9'},
+  ['1'] = {function() focus_winnr(1) end, 'Focus window 1'},
+  ['2'] = {function() focus_winnr(2) end, 'Focus window 2'},
+  ['3'] = {function() focus_winnr(3) end, 'Focus window 3'},
+  ['4'] = {function() focus_winnr(4) end, 'Focus window 4'},
+  ['5'] = {function() focus_winnr(5) end, 'Focus window 5'},
+  ['6'] = {function() focus_winnr(6) end, 'Focus window 6'},
+  ['7'] = {function() focus_winnr(7) end, 'Focus window 7'},
+  ['8'] = {function() focus_winnr(8) end, 'Focus window 8'},
+  ['9'] = {function() focus_winnr(9) end, 'Focus window 9'},
   ['/'] = {'<cmd>nohlsearch<cr>', 'No search highlight'},
   ['.'] = {'<cmd>Telescope buffers<cr>', 'Find buffer'},
   [':'] = {'<cmd>Telescope commands __hide_previewer=true<cr>', 'List commands'},
