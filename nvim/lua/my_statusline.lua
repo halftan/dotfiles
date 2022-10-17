894927c583fedbf261c60338f6a7aab3b8a446ff
Andy Zhang <half.a.tan@gmail.com>
2022-10-17 17:09:46 +0800

    Use breadcrumb

--- nvim/lua/my_statusline.lua
+++ nvim/lua/my_statusline.lua
@@ -7,5 +7,12 @@ call airline#add_statusline_func('default#add_winnr_statusline')
 call airline#add_inactive_statusline_func('default#add_winnr_statusline')
 let g:airline_theme='onedark'
 ]]
--- call airline#parts#define('winnr', { 'raw': '%{winnr()-1}', 'accent': 'none'})
+vim.fn['airline#parts#define_raw']('breadcrumb', "%{%v:lua.require'nvim-navic'.get_location()%}")
+
+if vim.fn.exists("+autochdir") and vim.o.autochdir then
+  vim.g.airline_section_c = vim.fn['airline#section#create_left']({'path', 'breadcrumb'})
+else
+  vim.g.airline_section_c = vim.fn['airline#section#create_left']({'file', 'breadcrumb'})
+end
+-- call airline#parts#define('breadcrumb', { 'raw': '%{winnr()-1}', 'accent': 'none'})
 -- let w:airline_section_a = airline#section#create_left(['winnr', 'mode', 'crypt', 'paste', 'keymap', 'spell', 'capslock', 'xkblayout', 'iminsert'])
