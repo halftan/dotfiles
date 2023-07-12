27d0d584fd03b7aed2206a2f57fc81d4117a0aae
Andy Zhang <half.a.tan@gmail.com>
2023-07-12 16:57:01 +0800

    update

--- nvim/lua/my_keymaps/space/space.lua
+++ nvim/lua/my_keymaps/space/space.lua
@@ -4,8 +4,8 @@ local utils = require('my_utils')
 wk.register({
   ['<space>'] = {
     name = 'Hop',
-    ['j'] = {'<cmd>HopLineStartAC<cr>', 'Line below'},
-    ['k'] = {'<cmd>HopLineStartBC<cr>', 'Line above'},
+    ['j'] = {'<cmd>HopVerticalAC<cr>', 'Line below'},
+    ['k'] = {'<cmd>HopVerticalBC<cr>', 'Line above'},
     ['/'] = {'<cmd>HopChar2<cr>', '2 char jump'},
     ['f'] = {'<cmd>HopChar1AC<cr>', '1 char forward'},
     ['F'] = {'<cmd>HopChar1BC<cr>', '1 char backward'},
