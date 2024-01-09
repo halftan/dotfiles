5b34ae0138125bf98b452c9bf17a45c221abcc10
Andy Zhang <half.a.tan@gmail.com>
2024-01-09 15:41:16 +0800

    Update

--- nvim/lua/my_treesitter_config.lua
+++ nvim/lua/my_treesitter_config.lua
@@ -108,6 +108,16 @@ return {
         enable = true,
       },
 
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn", -- set to `false` to disable one of the mappings
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+
     }
   end
 }
