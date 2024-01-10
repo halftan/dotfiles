d619b5722380f3201d615f2db06fa26f030d5fc1
Andy Zhang <half.a.tan@gmail.com>
2024-01-10 11:27:12 +0800

    update

--- nvim/lua/my_keymaps/space/_.lua
+++ nvim/lua/my_keymaps/space/_.lua
@@ -1,11 +1,11 @@
 local wk = require('which-key')
 local utils = require('my_utils')
 
-wk.register({
-  ['_'] = {
-    name = 'Example keymaps',
-    ['e'] = {'<cmd>echo ___<cr>', 'Example keymap for _'}
-  }
-}, { prefix = '<space>' })
+-- wk.register({
+--   ['_'] = {
+--     name = 'Example keymaps',
+--     ['e'] = {'<cmd>echo ___<cr>', 'Example keymap for _'}
+--   }
+-- }, { prefix = '<space>' })
 
 return {}
