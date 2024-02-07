03f68e0b7e11c4b78e8efed3b32069fb665a14e9
Andy Zhang <half.a.tan@gmail.com>
2024-02-07 15:32:47 +0800

    update

--- nvim/lua/my_plugins.lua
+++ nvim/lua/my_plugins.lua
@@ -3,6 +3,11 @@
 local pluginSpecs = {
   {
     'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
     opts = {
       plugins = {
         spelling = { enabled = true },
