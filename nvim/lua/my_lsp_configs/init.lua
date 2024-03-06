f2effcfe2286f724773986fc734476d30ce9b9e6
Andy Zhang <half.a.tan@gmail.com>
2024-03-06 11:10:34 +0800

    update nvim plugins

--- nvim/lua/my_lsp_configs/init.lua
+++ nvim/lua/my_lsp_configs/init.lua
@@ -8,13 +8,13 @@ local signature_setup = {
 }
 
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            -- apply whatever logic you want (in this example, we'll only use null-ls)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+  vim.lsp.buf.format({
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
 end
 
 -- if you want to set up formatting on save, you can use this as a callback
@@ -22,16 +22,16 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
 end
 
 local function is_null_ls_formatting_enabed(bufnr)
@@ -61,7 +61,9 @@ local on_attach_func = function(client, bufnr)
     then
       vim.api.nvim_buf_set_option(bufnr, 'formatexpr', 'v:lua.vim.lsp.formatexpr()')
     else
-      vim.api.nvim_buf_set_option(bufnr, 'formatexpr', nil)
+      if vim.api.nvim_get_option('formatexpr') ~= "" then
+        vim.api.nvim_buf_set_option(bufnr, 'formatexpr', nil)
+      end
     end
   end
 end
@@ -211,7 +213,7 @@ M.setup = function(pluginSpecs, ensure_capabilities)
         -- The first entry (without a key) will be the default handler
         -- and will be called for each installed server that doesn't have
         -- a dedicated handler.
-        function (server_name)
+        function(server_name)
           local ok, lsp_conf = pcall(require, 'my_lsp_configs.' .. server_name)
           if not ok then
             lsp_conf = {}
