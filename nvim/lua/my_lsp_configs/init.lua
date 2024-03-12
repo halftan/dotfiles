068ec9d3aedab36acf0ca3626af94904cdbad62d
Andy Zhang <half.a.tan@gmail.com>
2024-03-12 15:39:51 +0800

    update

--- nvim/lua/my_lsp_configs/init.lua
+++ nvim/lua/my_lsp_configs/init.lua
@@ -155,54 +155,7 @@ M.setup = function(pluginSpecs, ensure_capabilities)
       local configs = require 'lspconfig.configs'
       local lspconf = require 'lspconfig'
       if vim.fn.executable('appleconnect') == 1 then
-        configs.apple_codelm_ls = {
-          default_config = {
-            cmd = {
-              'apple-codelm-ls',
-              'start',
-              '--code-completion-max-prompt-tokens',
-              '2000',
-              -- server arguments go here (view args with `apple-codelm-ls start --help`)
-              -- ex:
-              '--max-completion-lines',
-              '10',
-              '--max-completion-tokens',
-              '100',
-              '--max-code-action-lines',
-              '20',
-              '--max-code-action-tokens',
-              '500',
-              'stdio',
-            },
-            filetypes = {
-              -- code formats
-              'c',
-              'cplusplus',
-              'csharp',
-              'go',
-              'java',
-              'javascript',
-              'objective-c',
-              'objective-cpp',
-              'php',
-              'python',
-              'ruby',
-              'swift',
-              'typescript',
-              'lua',
-              'vimscript',
-              -- text formats
-              'text',
-              'markdown',
-            },
-            single_file_support = true,
-          },
-          docs = {
-            description = [[
-              LSP server providing completions from CodeLM
-            ]],
-          },
-        }
+        configs.apple_codelm_ls = require('my_lsp_configs.apple_codelm_ls')
         lspconf.apple_codelm_ls.setup(
           ensure_capabilities(add_on_attach({}))
         )
